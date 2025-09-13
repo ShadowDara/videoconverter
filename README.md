5d81334f8f893512418fbb27ea2a752740aa0ecd
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-09-14 00:55:07 +0200

    add starter Files

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 to easily convert videos with ffmpge, for more
 infos read INFO.txt
+
+JAR File needs jar from dependencies.txt in the same Folder. Use `videoc` or `videoc.cmd` to start
---
+++ videoc.cmd
@@ -0,0 +1,9 @@
+SET SCRIPT_DIR=%~dp0
+
+ECHO Programm Requires Java 21
+
+SET LIB_DIR=%SCRIPT_DIR%
+
+SET CLASSPATH=%LIB_DIR%videoconverter.jar;%LIB_DIR%kotlin-stdlib-1.9.0.jar;%LIB_DIR%kotlinx-serialization-core-jvm-1.9.0.jar;%LIB_DIR%kotlinx-serialization-json-jvm-1.9.0.jar;%LIB_DIR%daras_library-0.1.4-SNAPSHOT.jar
+
+java -cp "%CLASSPATH%" de.shadowdara.videoconverter.MainKt
