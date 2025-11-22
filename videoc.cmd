9015158e2ba326c636f06419418cfbd183a09f9a
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-11-22 17:46:28 +0100

    Update videoc.cmd

--- videoc.cmd
+++ videoc.cmd
@@ -4,6 +4,6 @@ ECHO Programm Requires Java 21
 
 SET LIB_DIR=%SCRIPT_DIR%
 
-SET CLASSPATH=%LIB_DIR%videoconverter-0.1.4-SNAPSHOT.jar;%LIB_DIR%kotlin-stdlib-1.9.0.jar;%LIB_DIR%kotlinx-serialization-core-jvm-1.9.0.jar;%LIB_DIR%kotlinx-serialization-json-jvm-1.9.0.jar;%LIB_DIR%daras_library-0.1.5-SNAPSHOT.jar
+SET CLASSPATH=%LIB_DIR%videoconverter.jar;%LIB_DIR%kotlin-stdlib-1.9.0.jar;%LIB_DIR%kotlinx-serialization-core-jvm-1.9.0.jar;%LIB_DIR%kotlinx-serialization-json-jvm-1.9.0.jar;%LIB_DIR%daras_library-0.1.5-SNAPSHOT.jar
 
 java -cp "%CLASSPATH%" de.shadowdara.videoconverter.MainKt %*
